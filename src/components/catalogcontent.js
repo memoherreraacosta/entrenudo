313e0c34459a7bc0b314dcbeb1c49d9b9327112d
Karol José Gutiérrez Suárez <kjgs.22@gmail.com>
2020-09-28 08:42:08 -0500

    Change color when select paleta

--- src/components/catalogcontent.js
+++ src/components/catalogcontent.js
@@ -2,44 +2,62 @@ import React,  { useState } from "react";
 import Card from 'react-bootstrap/Card'
 import CardColumns from 'react-bootstrap/CardColumns'
 
-const CatalogContent = () => {
-    const [texto, setTexto] = useState(0);
 
-    const catalogData = [
-        {
-            title: "Summer rise",
-            src: "images/catalog/summer_rise_1.jpg",
-            text: "Texto 1"
-        },
-        {
-            title: "Warm hug",
-            src: "images/catalog/warm_hug_1.jpg",
-            text: "Texto 2"
-        },
-        {
-            title: "Serenity",
-            src: "images/catalog/serenity_1.jpg",
-            text: "Texto 3"
-        }
-    ];
+class CatalogContent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        selectedPaleta: -1,
+        step: 0,
+        count: 0,
+        texto: "hola mundo",
+        catalogData: [
+            {
+                title: "Summer rise",
+                src: "images/catalog/summer_rise_1.jpg",
+                text: "Texto 1"
+            },
+            {
+                title: "Warm hug",
+                src: "images/catalog/warm_hug_1.jpg",
+                text: "Texto 2"
+            },
+            {
+                title: "Serenity",
+                src: "images/catalog/serenity_1.jpg",
+                text: "Texto 3"
+            }
+        ],
 
-    const addToCart = (paleta) =>{
-        console.log("Producto añadido al carrito")
-        console.log(paleta)
-        fetch('http://localhost:8080/api/order')
-        .then(response => response.json())
-        .then(data => {
-            setTexto(JSON.stringify(data));
-        });
-        localStorage.setItem("paleta", paleta);
+      };
     }
     
-    return (
+    addToCart = (paleta, index) =>{
+        this.setState({
+            selectedPaleta: index
+        })
+        localStorage.setItem("paleta", paleta);
+    }
+
+    setBgPaleta = (index) =>{
+        console.log(this.state.selectedPaleta, index)
+        return index === this.state.selectedPaleta ? "success" : ""
+    }
+
+    setColorPaleta = (index) =>{
+        console.log(this.state.selectedPaleta, index)
+        return index === this.state.selectedPaleta ? "white" : ""
+    }
+
+
+
+    render() {
+        return (
         <div>
             <CardColumns>
-                {   catalogData.map( el =>{
+                {   this.state.catalogData.map( (el, index) =>{
                     return (
-                        <Card key={el.title} className="text-center" onClick={ () => addToCart(el.title)}>
+                        <Card key={el.title} className="text-center" bg={this.setBgPaleta(index)} text={this.setColorPaleta(index)} onClick={ () => this.addToCart(el.title, index)}>
                             <Card.Body >
                                 <Card.Title >{el.title}</Card.Title>
                                 <Card.Img variant="top" src={el.src} />
@@ -50,10 +68,10 @@ const CatalogContent = () => {
 
                 }
             </CardColumns>
-            <p>{texto}</p>
+            <p>{this.state.texto}</p>
         </div>
-
-    );
+        );
+    }
   }
   
 export default CatalogContent;
