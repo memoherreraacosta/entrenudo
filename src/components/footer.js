f2e07283c18c1783b2926b774edb57803146fd7c
Karol José Gutiérrez Suárez <kjgs.22@gmail.com>
2020-11-23 15:12:27 -0600

    fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import style from "./footer.module.css"
 
 const Footer = ({ siteTitle }) => (
   <footer className={style.colophon}>
-    © {new Date().getFullYear()} {siteTitle}
+    <a href="https://www.instagram.com/entrenudo/" style={{marginRight: 100 + 'px'}}>© {new Date().getFullYear()}  {siteTitle}</a>
   </footer>
 )
 
